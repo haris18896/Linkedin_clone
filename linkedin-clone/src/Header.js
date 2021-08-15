4927462ee356df9dbffa617628fd6f805c480dcf
Haris Ahmad <haris18896@gmail.com>
2021-08-15 07:20:26 -0700

    Updating Header

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -4,11 +4,9 @@ import './Header.css'
 function Header() {
     return (
         <div className="header">
-            <h1>This is Header</h1>
 
             <div className="header__left">
                 <img scr="" alt="" />
-
                 <div className="header__search">
                     {/* Search Icon */}
                     <input type="text" placeholder="Search" />
